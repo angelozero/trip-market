df75e2f8686f6075d0834cc2955f0f0d4c8bb1d3
Angelo Zero <angelo.akm@gmail.com>
2020-11-18 21:52:20 -0300

    Deixando taplicativo responsivo para qualquer device

--- trip-market/trip-market/dao/TripDAO.swift
+++ trip-market/trip-market/dao/TripDAO.swift
@@ -15,7 +15,15 @@ class TripDAO: NSObject {
         let saturn = Trip(title: "Saturn", days: 5, price: "5.000.000.00,00", imagePath: "saturn.jpg")
         let mars = Trip(title: "Mars", days: 7, price: "3.000.000.00,00", imagePath: "mars.jpg")
         
-        let tripList:Array<Trip> = [moon, saturn, mars]
+        let moon1 = Trip(title: "Moon", days: 10, price: "1.000.000.00,00", imagePath: "moon.jpg")
+        let saturn1 = Trip(title: "Saturn", days: 5, price: "5.000.000.00,00", imagePath: "saturn.jpg")
+        let mars1 = Trip(title: "Mars", days: 7, price: "3.000.000.00,00", imagePath: "mars.jpg")
+        
+        let moon2 = Trip(title: "Moon", days: 10, price: "1.000.000.00,00", imagePath: "moon.jpg")
+        let saturn2 = Trip(title: "Saturn", days: 5, price: "5.000.000.00,00", imagePath: "saturn.jpg")
+        let mars2 = Trip(title: "Mars", days: 7, price: "3.000.000.00,00", imagePath: "mars.jpg")
+        
+        let tripList:Array<Trip> = [moon, saturn, mars, moon1, saturn1, mars1, moon2, saturn2, mars2]
         
         return tripList
     }
